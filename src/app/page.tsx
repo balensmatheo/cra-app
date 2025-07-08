986273328c0561ec4370688ad4e3708e378d4c53
Matheo <matheobalens@gmail.com>
2025-07-08 09:51:53 +0200

    🎨 Changement de la structure
    Structure du code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,10 @@ import { useCRAState, type Category, type CategoriesState, type DataState } from
 import { type SectionKey } from "../constants/categories";
 import { CATEGORY_OPTIONS, SECTION_LABELS, NAME_DEBOUNCE_DELAY } from "../constants/ui";
 import { isFutureMonth, isDuplicateCategory } from "../constants/validation";
+import { Amplify } from "aws-amplify"
+import outputs from "../../amplify_outputs.json"
+
+Amplify.configure(outputs)
 
 export default function Home() {
   const today = new Date();
